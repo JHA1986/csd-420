64774b1c3f3bd76f76627ee0936b398bb4aea603
Jonah Aney <jonahaney@gmail.com>
2025-09-28 12:57:06 -0500

    Updated module-9

---
+++ module-9/AneyDBTest/src/Select5.java
@@ -0,0 +1,51 @@
+/*
+ * Jonah Aney 09/28/25 CSD420 Module-9 Database Testing: Provided example file for selecting results
+
+ * Professor Darrell Payne
+ * Bellevue University
+ *
+ */
+import java.sql.*;
+
+public class Select5{
+
+  public static void main(String args[]){
+
+    try{
+
+      Connection con;
+
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      String url = "jdbc:mysql://localhost:3306/databasedb?";
+
+      con = DriverManager.getConnection(url + "user=student1&password=pass");
+
+      System.out.println("Connection established - now executing a select");
+
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM address33");
+
+     System.out.println("Received Results:");
+
+     int i = rs.getMetaData().getColumnCount();
+
+     while(rs.next()){
+
+       for(int x = 1; x <= i; ++x){
+
+         System.out.println(rs.getString(x));
+       }
+
+       System.out.println("");
+     }
+
+     stmt.close();
+     con.close();
+    }
+    catch (java.lang.Exception ex){
+
+      ex.printStackTrace();
+    }
+  }
+}
---
+++ module-9/AneyDBTest/src/aneydbtest/AneyDBTest.java
@@ -0,0 +1,15 @@
+/*
+ * Jonah Aney 09/28/25 CSD420 Module-9 Database Testing:
+ * .java file for running the database testing.
+ */
+
+public class AneyDBTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
